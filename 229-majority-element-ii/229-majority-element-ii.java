f7b41eac4c58925c451f15e631727bbf061582f5
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-04-07 16:56:25 +0530

    Time: 4 ms (63.10%), Space: 49.7 MB (55.46%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,14 +1,53 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer, Integer> res = new HashMap();
+        int max1 = 0;
+        int max2 = 0;
+        int count1 = 0;
+        int count2 = 0;
         int n = nums.length;
         for(int i = 0; i < n; i++){
-            res.put(nums[i], res.getOrDefault(nums[i], 0) + 1);
+            if(nums[i] == max1) count1++;
+            else if(nums[i] == max2) count2++;
+            else if(count1 == 0){
+                max1 = nums[i];
+                count1++;
+            }
+            else if(count2 == 0){
+                max2 = nums[i];
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
+        
         List<Integer> ans = new ArrayList<Integer>();
-        for(Map.Entry<Integer, Integer> itt : res.entrySet()){
-            if(itt.getValue() > n / 3) ans.add(itt.getKey());
+        int curr_c1 = 0;
+        int curr_c2 = 0;
+        
+        
+        for(int i = 0; i < n; i++){
+            if(nums[i] == max1) curr_c1++;
+            if(nums[i] == max2) curr_c2++;
         }
-        return ans;
+        
+        if(curr_c1 > n / 3 && curr_c2 > n / 3 && max1 != max2){
+            ans.add(max1);
+            ans.add(max2);
+            return ans;
+        }
+        
+        if(curr_c1 > n / 3){
+            ans.add(max1);
+            return ans;
+        }
+        
+        if(curr_c2 > n / 3){
+            ans.add(max2);
+            return ans;
+        }
+        
+        else return ans;
     }
 }
