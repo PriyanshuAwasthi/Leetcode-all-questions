71d2e3e94576c99f226a9a5b046d0eb95028d185
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-04-08 14:51:55 +0530

    Time: 2 ms (92.04%), Space: 46.1 MB (23.30%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -4,9 +4,10 @@ class Solution {
         int n = nums.length;
         int []ans = new int[2];
         for(int i = 0; i < n; i++){
-            if(temp.containsKey(target - nums[i])){
+            int n_target = target - nums[i];
+            if(temp.containsKey(n_target)){
                 ans[0] = i;
-                ans[1] = temp.get(target - nums[i]);
+                ans[1] = temp.get(n_target);
                 return ans;
             }
             else{
