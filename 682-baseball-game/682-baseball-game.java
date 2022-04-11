2091eee499202e564e7339565ae2ed3cd004f431
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-04-11 17:25:39 +0530

    Time: 4 ms (54.23%), Space: 41.9 MB (77.55%) - LeetHub

--- 682-baseball-game/682-baseball-game.java
+++ 682-baseball-game/682-baseball-game.java
@@ -2,7 +2,7 @@ class Solution {
     public int calPoints(String[] ops) {
         Stack<Integer> res = new Stack<>();
         int n = ops.length;
-        int sum = 0;
+
         for(int i = 0; i < n; i++){
             switch(ops[i]){
                 case "+":
@@ -10,27 +10,30 @@ class Solution {
                     int second_last = res.peek();
                     res.push(last);
                     res.push(last + second_last);
-                    sum += res.peek();
+               
                     break;
                     
                 case "D":
                     res.push(res.peek() * 2);
-                    sum += res.peek();
+
                     break;
                     
                 case "C":
-                    sum -= res.peek();
+                
                     res.pop();
                     break;
                     
                 default:
                     res.push(Integer.valueOf(ops[i]));
-                    sum += res.peek();
+                   
                     break;
             }
         }
         
-        
+        int sum = 0;
+        for(int a : res){
+            sum += a;
+        }
         return sum;
     }
 }
