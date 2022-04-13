e18e9bb4e20a2ef677c3d7793ee6856ddb7ee36e
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-04-13 23:00:15 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (82.78%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,38 +1,45 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int i, k = 0, l = 0;
-        int m = matrix.length;
-        int n = matrix[0].length;
- 
         List<Integer> res = new ArrayList<Integer>();
- 
-        while (k < m && l < n) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int top = 0;
+        int down = n - 1;
+        int left = 0;
+        int right = m - 1;
+       
+        
+        while(top <= down && left <= right){
+       
+                for(int i = left; i <= right; i++){
+                    res.add(matrix[top][i]);
+                }
+                top++;
+            
+         
+                for(int i = top; i <= down; i++){
+                    res.add(matrix[i][right]);
+                }
+                right--;
+            
+                if(top <= down){
+                    for(int i = right; i >= left; i--){
+                        res.add(matrix[down][i]);
+                    }
+                    
+                    down--;
+                    
+                }
+            if(left <= right){
+                for(int i = down; i >= top; i--){
+                        res.add(matrix[i][left]);
+                    }
+                left++;
+            }
+            
+                
+          
            
-            for (i = l; i < n; ++i) {
-                res.add(matrix[k][i]);
-            }
-            k++;
- 
-            for (i = k; i < m; ++i) {
-                res.add(matrix[i][n - 1]);
-            }
-            n--;
- 
-            
-            if (k < m) {
-                for (i = n - 1; i >= l; --i) {
-                    res.add(matrix[m - 1][i]);
-                }
-                m--;
-            }
- 
-            
-            if (l < n) {
-                for (i = m - 1; i >= k; --i) {
-                    res.add(matrix[i][l]);
-                }
-                l++;
-            }
         }
         return res;
     }
