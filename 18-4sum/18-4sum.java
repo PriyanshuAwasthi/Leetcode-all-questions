823395db2c186c7801abe49d7e727580d3d15530
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-05-04 01:19:55 +0530

    Time: 19 ms (72.36%), Space: 44.7 MB (48.89%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,36 +1,35 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        int n = nums.length;
-        if(n == 0 || n < 4) return res;
         Arrays.sort(nums);
+        int n = nums.length;
         for(int i = 0; i < n; i++){
             for(int j = i + 1; j < n; j++){
                 int left = j + 1;
                 int right = n - 1;
                 int new_target = target - (nums[i] + nums[j]);
+                
                 while(left < right){
-                    int rem_sum = nums[left] + nums[right];
-                    if(rem_sum > new_target) right--;
-                    else if(rem_sum < new_target) left++;
+                    if(nums[left] + nums[right] > new_target) --right;
+                    else if(nums[left] + nums[right] < new_target) ++left;
                     else{
-                        List<Integer> quad = new ArrayList<Integer>();
-                        quad.add(nums[i]);
-                        quad.add(nums[j]);
-                        quad.add(nums[left]);
-                        quad.add(nums[right]);
-                        res.add(quad);
-                        
-                        while(left < right && nums[left] == quad.get(2)) ++left;
-                        
-                        while(left < right && nums[right] == quad.get(3)) --right;
+                        List<Integer> res2 = new ArrayList<Integer>();
+                        res2.add(nums[i]);
+                        res2.add(nums[j]);
+                        res2.add(nums[left]);
+                        res2.add(nums[right]);
+                        res.add(res2);
+                    
+                    
+                        while(left < right && nums[left] == res2.get(2)) ++left;
+                        while(left < right && nums[right] == res2.get(2)) --right;
                     }
                 }
+                
                 while(j + 1 < n && nums[j] == nums[j + 1]) ++j;
             }
             while(i + 1 < n && nums[i] == nums[i + 1]) ++i;
         }
-        
         return res;
     }
 }
