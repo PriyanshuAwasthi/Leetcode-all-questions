c9670e0899c611af8ba0458fc92a2c87f41a750e
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-05-07 02:14:13 +0530

    Time: 3 ms (38.11%), Space: 57.5 MB (14.27%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.java
+++ 485-max-consecutive-ones/485-max-consecutive-ones.java
@@ -3,12 +3,11 @@ class Solution {
         int n = nums.length;
         int max = 0;
         int curr = 0;
-        int i = 0;
-        while(i < n){
+        
+        for(int i = 0; i < n; i++){
             if(nums[i] == 1) curr++;
             else curr = 0;
-            i++;
-            if(curr != 0) max = Math.max(curr, max);
+            max = Math.max(curr, max);
         }
         return max;
     }
