2243b86cccc6a3f68b7c5caba2645eb89a92a282
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-05-07 17:36:20 +0530

    Time: 2 ms (99.77%), Space: 60.9 MB (97.65%) - LeetHub

--- 456-132-pattern/456-132-pattern.java
+++ 456-132-pattern/456-132-pattern.java
@@ -1,22 +1,14 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
-        int n = nums.length;
-        int min_arr[] = new int[n];
-        int i = 1;
-        min_arr[0] = nums[0];
-        while(i < n){
-            if(nums[i] < min_arr[i - 1]) min_arr[i] = nums[i];
-            else min_arr[i] = min_arr[i - 1];
-            i++;
-        }
-        Stack<Integer> st = new Stack<>();
-        int j = n - 1;
-        while(j >= 0){
-            while(!st.isEmpty() && st.peek() <= min_arr[j]) st.pop();
-            if(!st.isEmpty() && st.peek() < nums[j]) return true;
-            st.push(nums[j]);
-            j--;
-        }
-        return false;
+       int n = nums.length, top = n, third = Integer.MIN_VALUE;
+	   if(n < 3) return false;
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (nums[i] < third) return true;
+        while (top < n && nums[i] > nums[top]) third = nums[top++];
+        nums[--top] = nums[i];
+    }
+    
+    return false; 
     }
 }
