89c11d9fe6e2fe76a503b0d31823d7fc889098e4
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-05-07 22:04:04 +0530

    Time: 2 ms (99.77%), Space: 60.9 MB (97.65%) - LeetHub

--- 456-132-pattern/456-132-pattern.java
+++ 456-132-pattern/456-132-pattern.java
@@ -1,34 +1,14 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
-		if (nums.length < 3)
-			return false;
-		// Monotonically decreasing stack
-		Stack<NumPair> stack = new Stack<>();
-		int currMin = nums[0];
-		for (int i = 1; i < nums.length; i++) {
-			while (!stack.isEmpty() && nums[i] >= stack.peek().num) {
-				stack.pop();
-			}
-			if (!stack.isEmpty() && nums[i] > stack.peek().minLeft) {
-				// No need to check if nums[i] (possible 'nums[k]') is smaller than top of stack (possible 'nums[j]')
-				// So, we directly check if minimum element (possible 'nums[i]') existed with minLeft
-				return true;
-			}
-			stack.add(new NumPair(nums[i], currMin));
-			currMin = Math.min(currMin, nums[i]);
-		}
-		return false;
+       int n = nums.length, top = n, third = Integer.MIN_VALUE;
+	   if(n < 3) return false;
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (nums[i] < third) return true;
+        while (top < n && nums[i] > nums[top]) third = nums[top++];
+        nums[--top] = nums[i];
     }
-}
-
-class NumPair {
-	int num;
-	int minLeft;
-
-	public NumPair(int num, int minLeft) {
-		super();
-		this.num = num;
-		this.minLeft = minLeft;
-	}
-
+    
+    return false; 
+    }
 }
