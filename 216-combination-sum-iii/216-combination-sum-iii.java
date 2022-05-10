4c9ed447a2706d8ada09b05f861a1c404b3beae5
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-05-10 23:47:57 +0530

    Time: 1 ms (73.61%), Space: 42 MB (17.44%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -1,15 +1,17 @@
 class Solution {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    List<Integer> res2 = new ArrayList<Integer>();
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
         int min = (k * (k + 1)) / 2;
         if(n < min) return res;
         
         
-        help_me(res, new ArrayList<Integer>(), k, n, 1);
+        help_me(k, n, 1);
         return res;
     }
     
-    private void help_me(List<List<Integer>> res, List<Integer> res2, int k, int n, int start){
+    private void help_me(int k, int n, int start){
         if(n == 0 && k == res2.size()){
             res.add(new ArrayList<>(res2));
             return;
@@ -17,7 +19,7 @@ class Solution {
         
         for(int j = start; j <= 9; j++){
             res2.add(j);
-            help_me(res, res2, k, n - j, j + 1);
+            help_me(k, n - j, j + 1);
             res2.remove(res2.size() - 1);
         }
     }
