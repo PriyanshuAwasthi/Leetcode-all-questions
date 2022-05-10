7aa473ccb8f14beb0cbdab5c4b68bf0d5f699312
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-05-11 00:29:59 +0530

    Time: 1 ms (92.39%), Space: 46.7 MB (59.20%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,26 +1,24 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int []left_max = new int[n];
-        int []right_max = new int[n];
+        int left = 0;
+        int right = n - 1;
+        int lm = 0;
+        int rm = 0;
+        int total = 0;
         
-        for(int i = n - 1; i >= 0; i--){
-            if(i == n - 1) right_max[i] = height[i];
-            else if(height[i] > right_max[i + 1]) right_max[i] = height[i];
-            else right_max[i] = right_max[i + 1];
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] >= lm) lm = height[left];
+                else total += lm - height[left];
+                left++;
+            }
+            else if(height[right] <= height[left]){
+                if(height[right] >= rm) rm = height[right];
+                else total += rm - height[right];
+                right--;
+            }
         }
-        
-        for(int i = 0; i < n; i++){
-            if(i == 0) left_max[i] = height[i];
-            else if(height[i] > left_max[i - 1]) left_max[i] = height[i];
-            else left_max[i] = left_max[i - 1];
-        }
-        
-        int count = 0;
-        for(int i = 1; i < n - 1; i++){
-            count += Math.min(left_max[i], right_max[i]) - height[i];
-        }
-        
-        return count;
+        return total;
     }
 }
