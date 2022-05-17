6f5f4cae0091103a160390b64b094b17a2dfdc49
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-05-17 15:36:43 +0530

    Time: 3 ms (38.72%), Space: 43.8 MB (64.87%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -3,29 +3,17 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         find_subset(0, new ArrayList<>(), res, nums);
-        Set<List> set1 = new HashSet<List>();
-        for(int i = 0; i < res.size(); i++){
-            set1.add(res.get(i));
-        }
-        List<List<Integer>> finl = new ArrayList<List<Integer>>();
-        Iterator<List> itt = set1.iterator();
-        while(itt.hasNext()){
-            finl.add(itt.next());
-        }
-        return finl;
+        
+        return res;
     }
     
     private void find_subset(int i, List<Integer> res2, List<List<Integer>> res, int[] nums){
-        if(i == nums.length){
-            res.add(new ArrayList<>(res2));
-            return;
+        res.add(new ArrayList<>(res2));
+        for(int a = i; a < nums.length; a++){
+            if(a != i && nums[a] == nums[a - 1]) continue;
+            res2.add(nums[a]);
+            find_subset(a + 1, res2, res, nums);
+            res2.remove(res2.size() - 1);
         }
-        
-        find_subset(i + 1, res2, res, nums);
-        res2.add(nums[i]);
-
-        find_subset(i + 1, res2, res, nums);
-        res2.remove(res2.size() - 1);
-
     }
 }
