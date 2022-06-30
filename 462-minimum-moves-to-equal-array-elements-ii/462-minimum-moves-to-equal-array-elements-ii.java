808135e857d847e1a948381eac58de86068fbd0a
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-06-30 22:51:49 +0530

    Time: 5 ms (66.60%), Space: 45.9 MB (42.98%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -5,6 +5,7 @@ class Solution {
         int ans = 0;
         int start = 0;
         int end = n - 1;
+        //intuition is that sort the array and find difference that will be the no. of min steps
         while(start != end && end > start) ans += nums[end--] - nums[start++];
         return ans;
     }
