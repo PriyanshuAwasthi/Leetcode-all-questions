b60ce70b1f10c61c58c67661edcb5fca34f34c12
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-07-04 19:22:45 +0530

    Time: 3 ms (89.35%), Space: 45.5 MB (50.51%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,6 +1,15 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> res = new ArrayList<List<String>>();
+        char [][]board = new char[n][n];
+        int lower_dia[] = new int[2 * n - 1];
+        int upper_dia[] = new int[2 * n - 1];
+        int left[] = new int[n];
+        for(char arr[] : board) Arrays.fill(arr, '.');
+        
+        helper(0, res, board, upper_dia, lower_dia, left);
+        return res;
+        /*List<List<String>> res = new ArrayList<List<String>>();
         char [][]b = new char[n][n];
         int l[] = new int[n];
         int l_d[] = new int[2 * n - 1];
@@ -12,10 +21,40 @@ class Solution {
         }
         
         helper(0, b, l, l_d, u_d, res);
-        return res;
+        return res;*/
     }
     
-    public void helper(int col, char b[][], int l[], int l_d[], int u_d[], List<List<String>> res){
+    public void helper(int col, List<List<String>> res, char board[][], int upper_dia[], int lower_dia[], int left[]){
+        if(col == board.length){
+            res.add(make(board));
+            return;
+        }
+        
+        for(int i = 0; i < board.length; i++){
+            if(left[i] == 0 && lower_dia[col + i] == 0 && upper_dia[board.length - 1 + col - i] == 0){
+                board[i][col] = 'Q';
+                left[i] = 1;
+                lower_dia[col + i] = 1;
+                upper_dia[board.length - 1 + col - i] = 1;
+                helper(col + 1, res, board, upper_dia, lower_dia, left);
+                board[i][col] = '.';
+                left[i] = 0;
+                lower_dia[col + i] = 0;
+                upper_dia[board.length - 1 + col - i] = 0;
+            }
+        }
+    }
+    
+    public List<String> make(char board[][]){
+        List<String> res2 = new ArrayList<>();
+        for(int i = 0; i < board.length; i++){
+            String temp = new String(board[i]);
+            res2.add(temp);
+        }
+        return res2;
+    }
+    
+    /*public void helper(int col, char b[][], int l[], int l_d[], int u_d[], List<List<String>> res){
         if(col == b.length){
             res.add(make(b));
             return;
@@ -43,5 +82,5 @@ class Solution {
             res2.add(temp);
         }
         return res2;
-    }
+    }*/
 }
