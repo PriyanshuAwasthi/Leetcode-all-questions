dc059fc1630273ac49c5ea15747ed109d7cea11e
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-07-06 12:38:31 +0530

    Time: 1 ms (45.21%), Space: 41 MB (41.71%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,6 +1,19 @@
 class Solution {
     public int fib(int n) {
+        if(n == 0) return 0;
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        return helper(n, dp);
+    }
+    
+    public int helper(int n, int []dp){
         if(n == 0 || n == 1) return n;
-        return fib(n - 1) + fib(n - 2);
+        else if(dp[n] != 0) return dp[n];
+        else{
+            int i = helper(n - 1, dp) + helper(n - 2, dp);
+            dp[n] = i;
+        }
+        return dp[n];
     }
 }
