2e71fe52a7b4b2e731fefcf9a35b2e47c9da081e
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-07-07 20:11:04 +0530

    Time: 20 ms (45.91%), Space: 50.3 MB (60.28%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,20 +1,16 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);    
+        Map<Integer, Integer> temp1 = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            temp1.put(nums[i], temp1.getOrDefault(nums[i], 0) + 1);
         }
-        
-       
-    
-        
-        Queue<Integer> maxheap = new PriorityQueue<>((a, b) -> freq.get(b) - freq.get(a));
-        for(int key : freq.keySet()){ maxheap.add(key); }
+        PriorityQueue<Integer> temp2 = new PriorityQueue<>((a, b) -> temp1.get(b) - temp1.get(a));
+        for(int ki : temp1.keySet()) temp2.offer(ki);
         
         int ans[] = new int[k];
+        
         for(int i = 0; i < k; i++){
-            ans[i] = maxheap.poll();
+            ans[i] = temp2.poll();
         }
         
         return ans;
