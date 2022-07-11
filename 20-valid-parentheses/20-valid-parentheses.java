bfd9ff66873ef7675e78a514b175fe9cc5a48532
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-07-11 23:51:51 +0530

    Time: 2 ms (90.14%), Space: 41.8 MB (72.02%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,30 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack1 = new Stack<Character>();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
-                stack1.push(s.charAt(i));
-            }
-            
-            else if(s.charAt(i) == ')'){
-                if(stack1.isEmpty()) return false;
-                if(stack1.peek() == '(') stack1.pop();
-                else return false;
-            }
-            
-            else if(s.charAt(i) == '}'){
-                if(stack1.isEmpty()) return false;
-                if(stack1.peek() == '{') stack1.pop();
-                else return false;
-            }
-            
-            else if(s.charAt(i) == ']'){
-                if(stack1.isEmpty()) return false;
-                if(stack1.peek() == '[') stack1.pop();
-                else return false;
-            }
+        Stack<Character> st = new Stack<Character>();
+       for(int i = 0; i < s.length(); i++){
+           if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') st.push(s.charAt(i));
+           else if(s.charAt(i) == ')'){
+               if(st.isEmpty()) return false;
+               else if(st.peek() == '(') st.pop();
+               else return false;
+           }
+           else if(s.charAt(i) == '}'){
+               if(st.isEmpty()) return false;
+               else if(st.peek() == '{') st.pop();
+               else return false;
+           }
+           else if(s.charAt(i) == ']'){
+               if(st.isEmpty()) return false;
+               else if(st.peek() == '[') st.pop();
+               else return false;
+           }
         }
-        if(stack1.isEmpty()) return true;
-        return false;
+        return st.isEmpty();
     }
 }
