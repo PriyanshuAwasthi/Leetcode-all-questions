347a2b82f414800978a4e34f5dea8e144c49b073
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-07-13 22:27:46 +0530

    Time: 59 ms (55.80%), Space: 43.1 MB (68.97%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,47 +1,24 @@
 class Solution {
+    int max_len;
+    int res_start;
     public String longestPalindrome(String s) {
-        // Fill a boolean DP array using "Gap Strategy" or "Diagonal Traversal".
-        
-        boolean[][] dp1 = new boolean[s.length()][s.length()];
-        int longg=0;
-        int li=0;
-        int ri=0;
-        for (int g = 0; g < dp1.length; g++) {
-            for (int i = 0, j = g; j < dp1.length; i++, j++) {
-                
-                if (g==0) {
-                    dp1[i][j] = true;
-                    longg=g+1; //whenever we encounter a true(pallindromic substring), we store it's gap size and i,j in a global variable and then change it only when we find a bigger gap(longer substring).
-                    li=i;
-                    ri=j;
-                }
-
-                else if (g == 1) {
-                    if (s.charAt(i) == s.charAt(j)) {
-                        dp1[i][j] = true;
-                        longg=g+1;
-                        li=i;
-                        ri=j;
-                    }
-                }
-
-                else {
-                    if (s.charAt(i) == s.charAt(j)) {
-                        if (dp1[i + 1][j - 1] == true) {
-                            dp1[i][j] = true;
-                            longg=g+1;
-                            li=i;
-                            ri=j;
-                        }
-                    }
-                    else {
-                        dp1[i][j] = false;
-
-                    }
-                }
-
-            }
+        int n = s.length();
+        if(n < 2) return s;
+        for(int i = 0; i < n; i++){
+            helper(s, i, i);
+            helper(s, i, i + 1);
         }
-        return s.substring(li,ri+1);   
+        return s.substring(res_start, res_start + max_len);
+    }
+    
+    public void helper(String s, int start, int end){
+        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)){
+            start--;
+            end++;
+        }
+        if(max_len < end - start - 1){
+            max_len = end - start - 1;
+            res_start = start + 1;
+        }
     }
 }
