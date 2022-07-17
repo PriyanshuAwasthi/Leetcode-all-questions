a6d6d3b063ca63bf689d12cd15046eb9747199bb
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-07-17 23:39:44 +0530

    Time: 2 ms (85.17%), Space: 42.6 MB (28.83%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -2,9 +2,9 @@ class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         Boolean arr[][] = new Boolean[s1.length() + 1][s2.length() + 1];
         if(s1.length() + s2.length() != s3.length()) return false;
+        
         return helper(s1, s2, s3, 0, 0, arr);
     }
-    
     public boolean helper(String s1, String s2, String s3, int row, int col, Boolean arr[][]){
         if(row == s1.length() && col == s2.length()) return true;
         else if(arr[row][col] != null) return false;
