ba1895c9d41ece24db3645b71533b3eb9b92cf5a
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-07-21 21:56:52 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (9.16%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
@@ -12,6 +12,47 @@ class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         int count = 1;
         ListNode pre = null;
+        ListNode curr = null;
+        ListNode next = null;
+        ListNode dummy = head;
+        ListNode rnode = head;
+        while(count != right){
+            rnode = rnode.next;
+            count++;
+        }
+        count = 1;
+        if(left == count){
+            while(right >= count){
+                next = dummy.next;
+                dummy.next = (count == left) ? rnode.next : pre;
+                pre = dummy;
+                dummy = next;
+                count++;
+            }
+            head = pre;
+        }
+        else{
+            while(count != left - 1){
+                dummy = dummy.next;
+                count++;
+            }
+            count++;
+            curr = dummy.next;
+            while(right >= count){
+                next = curr.next;
+                curr.next = (count == left) ? rnode.next : pre;
+                pre = curr;
+                curr = next;
+                count++;
+            }
+            dummy.next = pre;
+        }
+        return head;
+    }
+}
+/*
+int count = 1;
+        ListNode pre = null;
         ListNode next = null;
         var rnode = head;
         ListNode dummy = head;
@@ -30,7 +71,6 @@ class Solution {
                 count++;
             }
             head = pre;
-            count = 1;
         }
         else{
             while(count != (left - 1)){
@@ -49,6 +89,4 @@ class Solution {
             dummy.next = pre;
         }
         return head;
-        
-    }
-}
+        */
