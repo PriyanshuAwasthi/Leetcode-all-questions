d48bb4146d1ec0a1e5dd8ac6f94ae98ffab90693
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-08-01 18:25:38 +0530

    Time: 1459 ms (5.00%), Space: 245.8 MB (5.09%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -2,7 +2,8 @@ class Solution {
     public int dp[][];
     public int count = 0;
     public int findTargetSumWays(int[] nums, int target) {
-        
+        dp = new int[1002][1002];
+        for(int []arr : dp) Arrays.fill(arr, - 1);
         helper(target, nums, 0, 0);
         return count;
     }
