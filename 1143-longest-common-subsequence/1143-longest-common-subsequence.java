15b9ba3eb6f17d2f0980ffb75bc3e4d3ad85156f
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-08-04 17:27:25 +0530

    Time: 507 ms (5.05%), Space: 365.6 MB (5.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,21 +1,16 @@
 class Solution {
     int dp[][];
     public int longestCommonSubsequence(String text1, String text2) {
-        dp = new int[text1.length()][text2.length()];
+        int n = text1.length();
+        int m = text2.length();
+        dp = new int[n + 1][m + 1];
         for(int arr[] : dp) Arrays.fill(arr, -1);
-        return helper(text1, text2, text1.length() - 1, text2.length() - 1);
+        return helper(text1, text2, n, m);
     }
-    public int helper(String text1, String text2, int i, int j){
-        if(i < 0 || j < 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        else if(text1.charAt(i) == text2.charAt(j)){
-            dp[i][j] = 1 + helper(text1, text2, i - 1, j - 1);
-            return dp[i][j];
-        }
-        int a = helper(text1, text2, i - 1, j);
-        int b = helper(text1, text2, i, j - 1);
-        if(a < b) dp[i][j] = 0 + b;
-        else dp[i][j] = 0 + a;
-        return dp[i][j];
+    public int helper(String text1, String text2, int n, int m){
+        if(n == 0 || m == 0) return dp[n][m] = 0;
+        if(dp[n][m] != -1) return dp[n][m];
+        if(text1.charAt(n - 1) == text2.charAt(m - 1)) return dp[n][m] = 1 + helper(text1.substring(0, n - 1), text2.substring(0, m - 1), n - 1, m - 1);
+        else return dp[n][m] = Math.max(helper(text1.substring(0, n - 1), text2, n - 1, m), helper(text1, text2.substring(0, m - 1), n, m - 1));
     }
 }
