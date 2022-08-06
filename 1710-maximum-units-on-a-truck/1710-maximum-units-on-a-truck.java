f3da83e7b950e1cd43a2f4ab39bb81d53cb2f9d0
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-08-06 17:33:01 +0530

    Time: 13 ms (56.78%), Space: 50.9 MB (45.23%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,21 +1,31 @@
 class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         //sort the array in ascending order according to the second column(i.e numberOfUnitsPerBox)
-        Arrays.sort(boxTypes, (a, b) -> Integer.compare(a[1],b[1]));
+        /*Arrays.sort(boxTypes, (a, b) -> Integer.compare(a[1],b[1]));
         
         int ans = 0;
-        for(int i=boxTypes.length-1;i>=0;i--)
-        {
-            if(truckSize == 0)
-                return ans;
-            if(boxTypes[i][0] <= truckSize)
-            {
+        for(int i=boxTypes.length-1;i>=0;i--){
+            if(truckSize == 0) return ans;
+            if(boxTypes[i][0] <= truckSize){
                 ans += boxTypes[i][0]*boxTypes[i][1];
                 truckSize -= boxTypes[i][0];
             }
-            else
-            {
+            else{
                 ans += truckSize*boxTypes[i][1];
+                truckSize = 0;
+            }
+        }
+        return ans;*/
+        Arrays.sort(boxTypes, (i, j) -> i[1] - j[1]);
+        int ans = 0;
+        for(int i = boxTypes.length - 1; i >= 0; i--){
+            if(truckSize == 0) break;
+            if(truckSize >= boxTypes[i][0]){
+                ans += boxTypes[i][0] * boxTypes[i][1];
+                truckSize -= boxTypes[i][0];
+            }
+            else{
+                ans += truckSize * boxTypes[i][1];
                 truckSize = 0;
             }
         }
