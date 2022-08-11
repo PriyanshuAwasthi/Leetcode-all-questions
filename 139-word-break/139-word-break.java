0b67fb3cfc5c7b00dc84806384a2e8879401f4d4
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-08-11 16:45:04 +0530

    Time: 15 ms (34.73%), Space: 46.5 MB (54.10%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,28 +1,26 @@
 class Solution {
-    Map<Integer, Boolean> mp;
+    //Map<Integer, Boolean> mp;
+    public int []dp;
     public boolean wordBreak(String s, List<String> wordDict) {
-        mp = new HashMap<>();
+        //mp = new HashMap<>();
+        dp = new int[s.length() + 1];
+        Arrays.fill(dp, -1);
         Set<String> temp = new HashSet<>();
         for(int i = 0; i < wordDict.size(); i++) temp.add(wordDict.get(i));
-        int ind = 0;
         if(temp.contains(s)) return true;
-        /*for(int i = 0; i < s.length(); i++){
-            if(temp.contains(s.substring(ind, i))){
-                return helper(s, i, temp);
-            }
-        }*/
         return helper(s, 0, temp);
     }
     public boolean helper(String s, int i, Set<String> temp){
         if(i == s.length()) return true;
-        if(mp.containsKey(i)) return mp.get(i);
+        //if(mp.containsKey(i)) return mp.get(i);
+        if(dp[i] != -1) return (dp[i] == 1) ? true : false;
         for(int j = i; j <= s.length(); j++){
             if(temp.contains(s.substring(i, j)) && helper(s, j, temp)){
-                mp.put(i, true);
+                dp[i] = 1;
                 return true;
             }
         }
-        mp.put(i, false);
+        dp[i] = 0;
         return false;
     }
 }
