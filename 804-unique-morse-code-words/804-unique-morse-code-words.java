c143040e5d91b94eaa34d15a57cd3e0bda3d7815
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-08-17 17:10:56 +0530

    Time: 4 ms (52.93%), Space: 42 MB (60.25%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.java
+++ 804-unique-morse-code-words/804-unique-morse-code-words.java
@@ -1,39 +1,14 @@
 class Solution {
+    public String []check;
     public int uniqueMorseRepresentations(String[] words) {
-        Map<Character, String> hm = new HashMap<>();
-        Set<String> check = new HashSet<>();
-        hm.put('a', ".-");
-        hm.put('b', "-...");
-        hm.put('c', "-.-.");
-        hm.put('d', "-..");
-        hm.put('e', ".");
-        hm.put('f', "..-.");
-        hm.put('g', "--.");
-        hm.put('h', "....");
-        hm.put('i', "..");
-        hm.put('j', ".---");
-        hm.put('k', "-.-");
-        hm.put('l', ".-..");
-        hm.put('m', "--");
-        hm.put('n', "-.");
-        hm.put('o', "---");
-        hm.put('p', ".--.");
-        hm.put('q', "--.-");
-        hm.put('r', ".-.");
-        hm.put('s', "...");
-        hm.put('t', "-");
-        hm.put('u', "..-");
-        hm.put('v', "...-");
-        hm.put('w', ".--");
-        hm.put('x', "-..-");
-        hm.put('y', "-.--");
-        hm.put('z', "--..");
-        for(int i = 0; i < words.length; i++){
-            String str = words[i];
-            String ans = "";
-            for(int j = 0; j < str.length(); j++) ans += hm.get(str.charAt(j));
-            check.add(ans);
-        }
-        return check.size();
+        check = new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        HashSet<String> set = new HashSet<String>();
+        for(String w : words) set.add(helper(w));
+        return set.size();
+    }
+    public String helper(String word){
+        String ans = "";
+        for(int i = 0; i < word.length(); i++) ans += check[(int)word.charAt(i) - 'a'];
+        return ans;
     }
 }
