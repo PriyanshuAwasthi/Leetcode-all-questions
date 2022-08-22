cfdb2c188eba667002bb6af777775e9a4459f52d
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-08-22 23:20:41 +0530

    Time: 2 ms (65.34%), Space: 41.4 MB (41.79%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,8 +1,18 @@
 class Solution {
-    public boolean isPowerOfFour(int n){
+    public boolean isPowerOfFour(int n) {
+        if(n <= 0) return false;
         if(n == 1) return true;
-        if(n == 0) return false;
-        if(n % 4 != 0) return false;
-        return isPowerOfFour(n / 4);
+        if(n > 1 && n < 4) return false;
+        long temp = 4;
+        while(temp <= n){
+            if(temp == n) return true;
+            else{
+                temp *= 4;
+                if(temp >= Integer.MAX_VALUE) break;
+            } 
+        }
+        
+        //System.out.print(1162261466.getClass().getName());
+        return false;
     }
 }
