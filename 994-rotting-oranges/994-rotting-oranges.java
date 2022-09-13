27653f2ff0f3f8dc93a649e4b9d7298db20cacac
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-09-14 01:58:15 +0530

    Time: 3 ms (76.26%), Space: 43.4 MB (26.49%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,51 +1,35 @@
 class Solution {
-    public int [][] dir=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+    public int [][] dir = new int[][]{{-1, 0},{1, 0},{0, -1},{0, 1}};
     public int orangesRotting(int[][] grid) {
-        
-        int m=grid.length;
-        int n=grid[0].length;
-        boolean flag=false;
-        Queue<Integer> q= new LinkedList<>();
-        for( int i=0;i<m;i++)
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==2)q.add(i*n+j);
-            }
-        
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean flag = false;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < m; i++) for(int j = 0; j < n; j++) if(grid[i][j] == 2) q.add(i * n + j);
         int level=0;
-        
         while(!q.isEmpty()){
             ++level;
-            int sz=q.size();
-            while(sz-->0){
+            int sz = q.size();
+            while(sz-- > 0){
                 
-                int curr=q.poll();
+                int curr = q.poll();
                 
-                int row=curr/n;
-                int col=curr%n;
-                for( int i=0;i<dir.length;i++){
-                    int r=row+dir[i][0];
-                    int c=col+dir[i][1];
+                int row = curr / n;
+                int col = curr % n;
+                for(int i = 0; i < dir.length; i++){
+                    int r = row + dir[i][0];
+                    int c = col + dir[i][1];
                     
-                    if(r>=0&&c>=0&&r<m&&c<n&&grid[r][c]==1){
-                        q.add(r*n+c);
-                        grid[r][c]=2;
-                        flag=true;
+                    if(r >= 0 && c >= 0 && r < m && c < n && grid[r][c] == 1){
+                        q.add(r * n + c);
+                        grid[r][c] = 2;
+                        flag = true;
                     }
-                }
-                 
+                }    
             }
-            
-              
-            
         }
-       
-         for( int i=0;i<m;i++)
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1)return -1;
-            }
-         if(flag==false)return 0;
-        
+        for(int i = 0; i < m; i++) for(int j = 0; j < n; j++) if(grid[i][j] == 1) return -1;
+        if(flag == false) return 0;
         return level-1;
-        
     }
 }
