a0d8c5ea2b1a9889e202a123da20b4a973344f75
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-09-16 15:01:26 +0530

    Time: 274 ms (50.40%), Space: 135.6 MB (19.64%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,4 +1,20 @@
-class Solution 
+class Solution {
+    public Integer dp[][];
+    public int maximumScore(int[] nums, int[] multipliers) {
+        int n = multipliers.length;
+        int m = nums.length;
+        dp = new Integer[n][n];
+        return helper(nums, multipliers, 0, m - 1, 0);
+    }
+    public int helper(int[] nums, int[] multipliers, int i, int j, int temp){
+        if(temp == multipliers.length) return 0;
+        if(dp[i][temp] != null) return dp[i][temp];
+        int l = nums[i] * multipliers[temp] + helper(nums, multipliers, i + 1, j, temp + 1);
+        int r = nums[j] * multipliers[temp] + helper(nums, multipliers, i, j - 1, temp + 1);
+        return dp[i][temp] = Math.max(l, r);
+    }
+}
+/*class Solution 
 {
     public int maximumScore(int[] nums, int[] multipliers) 
     {
@@ -21,4 +37,4 @@ class Solution
         
         return memo[left][idx]=Math.max(take_left,take_right);
     }
-}
+}*/
