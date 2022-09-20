f429632ddcdaecceb76ce679e19c9a2519311eec
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-09-20 21:05:35 +0530

    Time: 26 ms (99.55%), Space: 50.6 MB (87.90%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,4 +1,4 @@
-class Solution {    
+/*class Solution {    
     public int findLength(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
@@ -14,5 +14,78 @@ class Solution {
             }
         }
         return res;
+    }
+}*/
+class Solution {
+    private static final int PRIME = 101;
+    private static final int MOD = 1_000_000_007;
+    
+    public int findLength(int[] A, int[] B) {
+        if (A.length < B.length) return findLength(B, A);
+        
+        // 1.) Check result for length 1  <==>  single character
+        Set<Integer> set = new HashSet<>();
+        boolean lenZero = false;
+        
+        for (int a : A) set.add(a);
+        for (int b : B) {
+            if (!set.contains(b)) continue;
+            lenZero = true;
+            break;
+        }
+        
+        if (!lenZero) return 0;
+        
+        
+        // 2). Now the result lies in [1, min(A.length, B.length)], perform binary serach
+        int left = 1, right = Math.min(A.length, B.length);
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (check(mid, A, B)) left = mid;
+            else right = mid - 1;
+        }
+        return check(right, A, B) ? right : left;
+    }
+    
+    // check every subarray with length mid
+    private boolean check(int mid, int[] A, int[] B) {
+        Set<Integer> set = new HashSet<>();  
+        
+        // 1). Adding hash values of subarrays of B into set
+        long hash = 0;
+        for (int j = 0; j < mid; j++) {
+            hash = hash * PRIME + (B[j] + 1);
+            hash %= MOD;
+        }
+        set.add((int) hash);
+        
+        long power = 1;
+        for (int i = 0; i < mid-1; i++) {
+            power = (power * PRIME) % MOD;
+        }
+        
+        for (int i = 0, j = mid; j < B.length; i++, j++) {
+            hash = hash - ((B[i] + 1) * power % MOD) + MOD;
+            hash = hash * PRIME + (B[j] + 1);
+            hash %= MOD;
+            set.add((int) hash);
+        }
+        
+        // 2.) Check the hash values of subarrays of A
+        hash = 0;
+        for (int j = 0; j < mid; j++) {
+            hash = hash * PRIME + (A[j] + 1);
+            hash %= MOD;
+        }
+        if (set.contains((int)hash)) return true;
+        
+        for (int i = 0, j = mid; j < A.length; i++, j++) {
+            hash = hash - ((A[i] + 1) * power % MOD) + MOD;
+            hash = hash * PRIME + (A[j] + 1);
+            hash %= MOD;
+            if (set.contains((int)hash)) return true;
+        }
+        
+        return false;
     }
 }
