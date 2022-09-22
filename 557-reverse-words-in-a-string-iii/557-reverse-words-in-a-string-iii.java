29a34a1e151d315eded897986738d5b7ad3cf8ed
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-09-22 18:30:53 +0530

    Time: 7 ms (81.14%), Space: 49 MB (73.97%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -12,29 +12,20 @@
 // }
 class Solution {
     public String reverseWords(String s) {
-        
         char[]charArr = s.toCharArray();
         int leftIndex = 0;    
         int spaceIndex = 0;
-        for(int i = 0;i<charArr.length;i++){
-            
-            if(i==charArr.length-1 || charArr[i]==' '){
-                
-                spaceIndex = i == charArr.length-1 ? i : i-1;
-                
-                reverse(charArr,leftIndex,spaceIndex);
-                
-                leftIndex = i+1;
+        for(int i = 0; i < charArr.length; i++){
+            if(i == charArr.length - 1 || charArr[i] == ' '){
+                spaceIndex = (i == charArr.length - 1) ? i : i - 1;
+                reverse(charArr, leftIndex, spaceIndex);
+                leftIndex = i + 1;
             }
-            
         }
-            
         return new String(charArr);
     }
-    
-    private void reverse(char[] cArr, int l,int r){
-        
-        while(l<r){            
+    private void reverse(char[] cArr, int l, int r){   
+        while(l < r){            
             char c = cArr[l];
             cArr[l++] = cArr[r];
             cArr[r--] = c; 
