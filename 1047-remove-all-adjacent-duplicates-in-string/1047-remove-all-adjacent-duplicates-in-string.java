416addae4563eaeafc708cf56150466404fad831
PriyanshuAwasthi <69391235+PriyanshuAwasthi@users.noreply.github.com>
2022-11-10 21:03:15 +0530

    Time: 104 ms (51.35%), Space: 55.7 MB (43.64%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,13 +1,25 @@
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         Stack<Character> s1 = new Stack<>();
+//         for(int i = 0; i < s.length(); i++){
+//             if(s1.size() >= 1 && s.charAt(i) == s1.peek()) s1.pop();
+//             else s1.push(s.charAt(i));
+//         }
+//         StringBuilder ans = new StringBuilder();
+//         Iterator<Character> value1 = s1.iterator();
+//         while(value1.hasNext()) ans.append(value1.next().toString());
+//         return ans.toString();
+//     }
+// }
 class Solution {
     public String removeDuplicates(String s) {
-        Stack<Character> s1 = new Stack<>();
+        Stack<Character> temp = new Stack<>();
         for(int i = 0; i < s.length(); i++){
-            if(s1.size() >= 1 && s.charAt(i) == s1.peek()) s1.pop();
-            else s1.push(s.charAt(i));
+            if(temp.size() >= 1 && temp.peek() == s.charAt(i)) temp.pop();
+            else temp.push(s.charAt(i));
         }
-        StringBuilder ans = new StringBuilder();
-        Iterator<Character> value1 = s1.iterator();
-        while(value1.hasNext()) ans.append(value1.next().toString());
-        return ans.toString();
+        StringBuilder sb =  new StringBuilder();
+        while(!temp.isEmpty()) sb.append(temp.pop());
+        return sb.reverse().toString();
     }
 }
